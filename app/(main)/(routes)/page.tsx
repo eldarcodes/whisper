5c62d20132d5a7657dfdf30ab810bd146188f023
eldarcodes <prog.eldar@gmail.com>
2023-09-07 21:32:45 +0300

    feat(auth): setup authentication using clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,19 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="container py-10">
-      <p className="text-3xl font-bold text-indigo-500 mb-2">Whisper</p>
-
-      <div className="flex gap-3">
-        <Link href="/register">
-          <Button variant="default">Sign up</Button>
-        </Link>
-
-        <Link href="/login">
-          <Button variant="outline">Login</Button>
-        </Link>
+      <div>
+        <UserButton afterSignOutUrl="/" />
       </div>
     </div>
   );
