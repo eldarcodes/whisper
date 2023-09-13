a0e29568e5b2280ee775e4caae000835fd0d3b64
eldarcodes <prog.eldar@gmail.com>
2023-09-13 21:30:09 +0300

    feat(api): create whisper server api

---
+++ app/api/servers/route.ts
@@ -0,0 +1,47 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { MemberRole } from "@prisma/client";
+import { NextResponse } from "next/server";
+import * as uuid from "uuid";
+
+export async function POST(req: Request) {
+  try {
+    const { name, imageUrl } = await req.json();
+
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const server = await db.server.create({
+      data: {
+        profileId: profile.id,
+        name,
+        imageUrl,
+        inviteCode: uuid.v4(),
+        channels: {
+          create: [
+            {
+              name: "General",
+              profileId: profile.id,
+            },
+          ],
+        },
+        members: {
+          create: [
+            {
+              profileId: profile.id,
+              role: MemberRole.ADMIN,
+            },
+          ],
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.error("Server POST error:", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
