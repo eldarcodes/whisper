9769c64cce65b699c6b73d508029129ece347f31
eldarcodes <prog.eldar@gmail.com>
2023-09-26 22:15:52 +0300

    feat(ui): add modal store using zustand and add modal provider to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import "./globals.css";
+
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/providers/theme-provider";
+
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -21,6 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+          <ModalProvider />
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { CreateServerModal } from "@/components/modals/create-server-modal";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // resolve Hydration of the initial UI error
+  useEffect(() => {
+    setIsMounted(true);
+
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <CreateServerModal />
+    </>
+  );
+};
