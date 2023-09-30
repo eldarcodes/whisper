d7148332a0974a3e0149cd7c5a0e099c3d6a4209
eldarcodes <prog.eldar@gmail.com>
2023-09-30 21:45:59 +0300

    feat(invitations): add invite code page with redirect to server (if user is new for this server)

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -75,8 +75,6 @@ export const CreateServerModal = () => {
 
   return (
     <Dialog open={isModalOpen} onOpenChange={handleClose}>
-      <DialogTrigger>Open</DialogTrigger>
-
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
