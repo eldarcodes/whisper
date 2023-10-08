c4a3a557ac10d60655c4309eb47edb2054d069f6
eldarcodes <prog.eldar@gmail.com>
2023-10-08 21:34:53 +0300

    feat(conversations): create or get initial conversation on member chat page

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
+import { UserAvatar } from "@/components/user-avatar";
 
 interface ChatHeaderProps {
   name: string;
@@ -20,6 +21,10 @@ export const ChatHeader = ({
 
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
       )}
 
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
